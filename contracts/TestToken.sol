2a12eef72a677449667f6ce9e9489600ffcdc9fd
ericbba <eric@blockei.io>
2023-04-10 02:28:04 -0400

    rename test token

--- contracts/TestToken.sol
+++ contracts/TestToken.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract ERC20Token is ERC20 {
-    constructor() ERC20("ERC20Token", "ERT") {
+contract TestToken is ERC20 {
+    constructor() ERC20("Test Token", "TT") {
         _mint(msg.sender, 10 ** 9 * 10 ** 18);
     }
 }
